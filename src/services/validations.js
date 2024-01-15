f3920231a78305f8c6e79086c2401d50aacc1bbf
Rekaya Nour <nourrkaya@live.be>
2024-01-15 17:21:28 +0100

    feat/add: test 'should return false if gamertag length is less than 8 characters' succeded

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,7 +1,13 @@
 // TODO: Refactor validation function to follow the defined validations rules
 const isValid = () => Math.random() >= 0.5;
 
-
+function isValidGamertag(gamertag) {
+    if (gamertag.length < 8) {
+        console.log("Invalid - gamertag length must be at least 8 characters");
+        return false;
+    }
+    return true; 
+}
 
 
 exports.isValid = isValid;
